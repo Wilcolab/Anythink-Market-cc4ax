4f17cac410526d176e96b03a0e0230d4e05078c1
Alon Carmel <alon.carmel@gmail.com>
2022-11-23 19:42:23 +0000

    Adding open ai imge url

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -4,7 +4,15 @@ var Item = mongoose.model("Item");
 var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
+
 const { sendEvent } = require("../../lib/event");
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
 
 // Preload item objects on routes with ':item'
 router.param("item", function(req, res, next, slug) {
@@ -139,12 +147,28 @@ router.get("/feed", auth.required, function(req, res, next) {
 
 router.post("/", auth.required, function(req, res, next) {
   User.findById(req.payload.id)
-    .then(function(user) {
+    .then(async function(user) {
       if (!user) {
         return res.sendStatus(401);
       }
+      item = req.body.item;
 
-      var item = new Item(req.body.item);
+      //Adding API KEY to generate new image
+
+      const response = await openai.createImage({
+        prompt: item.title,
+        n: 1,
+        size: "1024x1024",
+      });
+      image_url = response.data.data[0].url;
+      console.log(response);
+      if(item.image == '') {
+        item.image = image_url;
+      }
+
+      console.log(image_url);
+      console.log(item);
+      var item = new Item(item);
 
       item.seller = user;
 
