bfe0cc2f1b1f4c90e835697799504046fc19aaf0
Alon Carmel <alon.carmel@gmail.com>
2022-12-10 00:26:11 +0000

    again

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,7 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
-  if(!Object.prototype.hasOwnProperty.call(item, "image")) {
+  if(!props.item.image) {
     item.image = "/placeholder.png";
   }
   const handleClick = (ev) => {
