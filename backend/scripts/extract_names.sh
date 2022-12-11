c6f8495cd86da35dff5640780c8b9c2ce384eba5
Alon Carmel <alon.carmel@gmail.com>
2022-12-11 18:15:04 +0000

    script

---
+++ backend/scripts/extract_names.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
+do
+  email=$(echo "$rec_column4" | tr '[:upper:]' '[:lower:]') 
+  if [[ $email == *"@amazon.com"* ]]; then
+    echo "$rec_column3 $rec_column2"
+  fi
+ 
+done < <(tail -n +2 $1)
