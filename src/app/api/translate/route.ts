65026d38bbec89ed0557de3ae29f87b452561533
Alon Carmel <alon.carmel@gmail.com>
2025-01-01 12:14:17 +0000

    chnage

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -1,36 +1,36 @@
-import { NextResponse } from 'next/server'
-import OpenAI from 'openai';
+  import { NextResponse } from 'next/server'
+  import OpenAI from 'openai';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
-//test
-export async function POST(request: Request) {
-  const { text, language } = await request.json();
-  
-  const response = await openai.chat.completions.create({
-    model: "gpt-4",
-    messages: [
-      {
-        "role": "system",
-        "content": `
-          You will be provided with a sentence. Your tasks are to:
-          - Detect what language the sentence is in
-          - Translate the sentence into ${language}
-          Do not return anything other than the translated sentence.
-        `
-      },
-      {
-        "role": "user",
-        "content": text
-      }
-    ],
-    temperature: 0.7,
-    max_tokens: 64,
-    top_p: 1,
-  });
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+  //test
+  export async function POST(request: Request) {
+    const { text, language } = await request.json();
+    
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          "role": "system",
+          "content": `
+            You will be provided with a sentence. Your tasks are to:
+            - Detect what language the sentence is in
+            - Translate the sentence into ${language}
+            Do not return anything other than the translated sentence.
+          `
+        },
+        {
+          "role": "user",
+          "content": text
+        }
+      ],
+      temperature: 0.7,
+      max_tokens: 64,
+      top_p: 1,
+    });
 
-  return NextResponse.json({
-    text: response.choices[0].message.content
-  });
-}
+    return NextResponse.json({
+      text: response.choices[0].message.content
+    });
+  }
