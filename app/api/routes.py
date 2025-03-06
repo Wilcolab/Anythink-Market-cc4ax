afbf904fe3da479e0df7941a2cea7fa887a4984b
Alon Carmel <alon.carmel@gmail.com>
2025-03-06 09:09:10 +0000

    test

--- app/api/routes.py
+++ app/api/routes.py
@@ -58,8 +58,12 @@ async def secure_query(
     intent_tag = llm_service.interpret_user_intent(query)
     
     logger.info(query);
+    
+    
     context = await get_context_for_intent(intent_tag, current_user.username)
     response = llm_service.generate_response(query, context)
+
+
     logger.info(response);
 
     return QueryResponse(response=response)
